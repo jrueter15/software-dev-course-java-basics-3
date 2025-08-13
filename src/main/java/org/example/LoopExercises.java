9a3187949ceb322d6565839759ea15c7d7813ac6
jrueter15 <45775156+jrueter15@users.noreply.github.com>
2025-08-12 20:11:56 -0500

    Completed loop exercises.

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -1,16 +1,28 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class LoopExercises {
     public int sum(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // (use a for loop)
-        return 0;
+        int total = 0;
+        for(int i = 0; i <= n; i++){
+            total += i;
+        }
+        return total;
     }
 
     public int sumUntilEven(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // but stops adding when the sum is even
         // (use a while loop with a sum variable and a counter variable)
-        return 0;
+        int total = 0;
+        int i = 1;
+        do{
+            total += i;
+            i++;
+        } while(i <= n && total % 2 != 0);
+        return total;
     }
 }
